ee9819337224fb67227e806f466bb664aa9fe2b1
Devanshprabhakar24 <dev24prabhakar@gmail.com>
2026-01-08 01:58:27 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -17,7 +17,7 @@ module.exports.registerUser = async function (req, res) {
 
         bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(password, salt, async function (err, hash) {
-                if (err) return res.send(err.message);
+                if (err) return res.status(500).json({ error: "Failed to hash password" });
                 else {
                     let user = await userModel.create({
                         email,
@@ -33,7 +33,7 @@ module.exports.registerUser = async function (req, res) {
                         maxAge: 7 * 24 * 60 * 60 * 1000,
                         path: '/'
                     });
-                    res.send("User registered successfully")
+                    res.redirect("/shop")
                 }
             })
         })
@@ -43,31 +43,41 @@ module.exports.registerUser = async function (req, res) {
     }
 }
 module.exports.loginUser = async function (req, res) {
-    let { email, password } = req.body;
-    let user = await userModel.findOne({ email: email })
-    if (!user) {
-        return res.send("Email or password is incorrect");
+    try {
+        let { email, password } = req.body;
+        let user = await userModel.findOne({ email: email })
+        if (!user) {
+            req.flash("error", "Email or password is incorrect");
+            return res.redirect("/");
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (err) {
+                console.error(err);
+                req.flash("error", "Login failed. Please try again.");
+                return res.redirect("/");
+            }
+            if (result) {
+                const token = generateToken(user);
+                const isProd = process.env.NODE_ENV === 'production';
+                res.cookie("token", token, {
+                    httpOnly: true,
+                    secure: isProd,
+                    sameSite: isProd ? 'lax' : 'lax',
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
+                    path: '/'
+                });
+                res.redirect("/shop");
+            }
+            else {
+                req.flash("error", "Email or password is incorrect");
+                return res.redirect("/");
+            }
+        })
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "An error occurred. Please try again.");
+        return res.redirect("/");
     }
-    bcrypt.compare(password, user.password, function (err, result) {
-        if (result) {
-            const token = generateToken(user);
-            const isProd = process.env.NODE_ENV === 'production';
-            res.cookie("token", token, {
-                httpOnly: true,
-                secure: isProd,
-                sameSite: isProd ? 'lax' : 'lax',
-                maxAge: 7 * 24 * 60 * 60 * 1000,
-                path: '/'
-            });
-            res.redirect("/shop");
-        }
-        else {
-            return res.send("Email or password is incorrect")
-        }
-
-    })
-
-
 }
 
 module.exports.logout = function (req, res) {
